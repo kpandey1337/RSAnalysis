a000153e46b510f4ea9c4dbccedf30b6ae3bf81a
kunal <kpandey@scu.edu>
2021-01-28 19:02:45 -0800

    Regex for Webscrapper
    Regex removes lua text from wiki api.

--- wikiwebscrapper.py
+++ wikiwebscrapper.py
@@ -1,9 +1,31 @@
 import requests
+import re
 
 
 url = "https://runescape.wiki/w/Module:Exchange/Fire_rune/Data?action=raw"
 page = requests.get(url)
 
-f= open("data.txt", "a")
-f.write(page.text)
+f= open("data.txt", "w")
+rsdata = page.text
+rsdata = re.sub('[return\{\'\}\\t]','',rsdata)
+f.write(rsdata)
 f.close()
+myfile = open("data.txt", "r")
+g=open("finishedData.txt", "w")
+
+
+for line in myfile:
+    if re.search('.*:.*:.*', line):
+        print(line)
+        line = re.sub('(:[0-9]*\.[0-9],)$', ',' , line)
+        print(line)
+        g.write(line)
+    else:
+        g.write(line)
+
+#(date:price:volume,)x
+#([0-9]+:[0-9]+(:*)?,)+
+
+
+# ^[0-9]+
+g.close()
